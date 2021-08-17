a232d03f6b5525644e284b37c3aa7bdbd61b4a0c
Eduardo Silva de Freitas Correa <eduardo.correa@first-tech.net>
2021-08-17 19:12:04 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department department = new Department(1, "Vendas");
+		System.out.println(department);
+
+	}
+
+}
